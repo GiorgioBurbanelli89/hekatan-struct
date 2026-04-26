1f60369541ded2f49885c5edbe1b5b520e24e9fc
Giorgio Burbanelli <jbjazzjuventus1989@gmail.com>
2026-04-26 07:33:38 -0500

    Cortes X/Y/Z universales en Settings + fix material.clippingPlanes

---
+++ assets/columna-cft-CKqkuplf.js
@@ -0,0 +1,26 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { c as m, __tla as __tla_0 } from "./columnaCft-CCneKoXg.js";
+import { r as o } from "./runExampleStandalone-CZQkwxSI.js";
+import { __tla as __tla_1 } from "./didacticCpp-l2dJQgN-.js";
+import "./fiberSectionCft-DmBufNsV.js";
+import "./theme-2eEBQPmF.js";
+import "./getViewer-BrlnwplQ.js";
+import "./Text-DyNVkyur.js";
+import "./styles-Cjdl64P4.js";
+import "./units-d0zQYQ9A.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  o(m);
+});
