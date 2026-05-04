d54adf031a7d9e585d734b6d770a1d40163f1c41
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-05-04 15:19:21 -0500

    deploy: hover sphere amarillo con escala constante

---
+++ assets/tower-3d-C6YlI28Y.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { t as r, __tla as __tla_0 } from "./tower3D-dr7ba2Fl.js";
+import { r as o, __tla as __tla_1 } from "./runExampleStandalone-Df4m_6ow.js";
+import "./analyze-DNPn2SjO.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-Ck1qafl6.js";
+import { __tla as __tla_3 } from "./deform-5xY_VJz5.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./theme-2eEBQPmF.js";
+import "./getViewer-DM73MuIE.js";
+import "./Text-BE8nxNWm.js";
+import "./styles-Cjdl64P4.js";
+import "./units-BHnVfatd.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  o(r);
+});
---
+++ assets/triangular-plate-DgXcdpQ7.js
@@ -0,0 +1,40 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { t as r, __tla as __tla_0 } from "./triangularPlate-B3UkFh-k.js";
+import { r as t, __tla as __tla_1 } from "./runExampleStandalone-Df4m_6ow.js";
+import { __tla as __tla_2 } from "./didacticCpp-Ck1qafl6.js";
+import { __tla as __tla_3 } from "./deform-5xY_VJz5.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./mitc3-2FJr2z_r.js";
+import "./theme-2eEBQPmF.js";
+import "./getViewer-DM73MuIE.js";
+import "./Text-BE8nxNWm.js";
+import "./styles-Cjdl64P4.js";
+import "./units-BHnVfatd.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  t(r);
+});
