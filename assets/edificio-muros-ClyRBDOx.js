6a02031e04ac3ac8f6cdef7933f6c25acf05bb74
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-05-04 14:58:05 -0500

    deploy: ejes Revit como tool (2 clicks start+end)

---
+++ assets/edificio-muros-ClyRBDOx.js
@@ -0,0 +1,48 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { e as o, __tla as __tla_0 } from "./edificioMuros-D1VNiu8A.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-DNjp9Y0C.js";
+import { __tla as __tla_2 } from "./edificioAporticado-RB5t-nM4.js";
+import "./analyze-DNPn2SjO.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_3 } from "./didacticCpp-Ck1qafl6.js";
+import { __tla as __tla_4 } from "./deform-5xY_VJz5.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./Text-BE8nxNWm.js";
+import "./theme-2eEBQPmF.js";
+import "./getViewer-DJJSYGo6.js";
+import "./styles-Cjdl64P4.js";
+import "./units-BHnVfatd.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(o);
+});
