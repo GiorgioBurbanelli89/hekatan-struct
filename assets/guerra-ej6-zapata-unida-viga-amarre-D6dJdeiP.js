16623ac8cdebbfbf8c1b3f359b3f81b4d91c3739
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-05-21 00:05:44 -0500

    feat(ej6): wrapper sobre zapata-viga-amarre existente con datos Guerra

--- assets/guerra-ej6-zapata-unida-viga-amarre-D6dJdeiP.js
+++ assets/guerra-ej6-zapata-unida-viga-amarre-D6dJdeiP.js
@@ -1,11 +1,15 @@
 import "./modulepreload-polyfill-B5Qt9EMX.js";
-import { g as r, __tla as __tla_0 } from "./guerraEj6-DD9NdlcG.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj6-DsGj3i_m.js";
 import { r as m, __tla as __tla_1 } from "./runExampleStandalone-BRuCCjKI.js";
+import { __tla as __tla_2 } from "./zapataVigaAmarre-CkGp7hM-.js";
 import "./Text-BmY6zyQy.js";
 import "./theme-2eEBQPmF.js";
-import { __tla as __tla_2 } from "./didacticCpp-CLixJGob.js";
-import { __tla as __tla_3 } from "./deform-C054iIuL.js";
+import "./analyze-DNPn2SjO.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_3 } from "./didacticCpp-CLixJGob.js";
+import { __tla as __tla_4 } from "./deform-C054iIuL.js";
 import "./preload-helper-DrUBW0xl.js";
+import "./exampleVersion-D1A_5i59.js";
 import "./getViewer-CoMhMl8V.js";
 import "./styles-lf_LNy9d.js";
 import "./units-yggW8zr_.js";
@@ -31,6 +35,12 @@ Promise.all([
   (() => {
     try {
       return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
     } catch {
     }
   })()
