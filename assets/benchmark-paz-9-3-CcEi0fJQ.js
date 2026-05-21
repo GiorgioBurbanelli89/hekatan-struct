47967c1ce82241d4106ca29843a07e16798c9b8c
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-05-20 23:46:01 -0500

    fix(ej6): mesh trim - 2 zapatas separadas visibles como libro Fig.181

---
+++ assets/benchmark-paz-9-3-CcEi0fJQ.js
@@ -0,0 +1,49 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { b as r, __tla as __tla_0 } from "./benchmarkPaz9_3-CisRI7Hv.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-BRuCCjKI.js";
+import { __tla as __tla_2 } from "./didacticCpp-CLixJGob.js";
+import { __tla as __tla_3 } from "./deform-C054iIuL.js";
+import "./preload-helper-DrUBW0xl.js";
+import { __tla as __tla_4 } from "./pazFrameE2k-BN1k77mc.js";
+import "./analyze-DNPn2SjO.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import "./newmarkBeta-CZ1uRmPu.js";
+import "./theme-2eEBQPmF.js";
+import "./getViewer-CoMhMl8V.js";
+import "./Text-BmY6zyQy.js";
+import "./styles-lf_LNy9d.js";
+import "./units-yggW8zr_.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
