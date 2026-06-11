3ba00d722416d54818573c99f5eea950c0b20805
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-10 23:07:25 -0500

    feat(cad): la barra de comandos acepta coordenadas (1,1,1 / @5,3 / 5<45) y coloca el punto

---
+++ assets/1d-mesh-n6QQufpv.js
@@ -0,0 +1,200 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { v as s } from "./theme-D5p5K0bJ.js";
+import { a as O } from "./analyze-DQyLT4zB.js";
+import { d as b, __tla as __tla_0 } from "./didacticCpp-C2di29sC.js";
+import { g as k } from "./getViewer-BhdEu_ed.js";
+import { g as S } from "./getParameters-DJgDJFZz.js";
+import { g as A } from "./styles-Bs20h4nQ.js";
+import { g as _, __tla as __tla_1 } from "./getCad3d-BSh01O8z.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./deform-D-nmLsId.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./Text-B4nrRMfX.js";
+import "./exampleVersion-D1A_5i59.js";
+import { __tla as __tla_3 } from "./getMesh-CMOA6JCi.js";
+import "./__vite-browser-external-D7Ct-6yo.js";
+import "./e2kExporter-BuERGNNd.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  const p = {
+    meshDensity: {
+      value: s.state(7),
+      min: 1,
+      max: 7,
+      step: 1,
+      label: "mesh density"
+    },
+    span: {
+      value: s.state(10),
+      min: 1,
+      max: 20
+    },
+    height: {
+      value: s.state(10),
+      min: 1,
+      max: 10
+    },
+    load: {
+      value: s.state(10),
+      min: 0,
+      max: 20
+    }
+  }, l = s.state([]), i = s.state([]), d = s.state({}), h = s.state({}), g = s.state({}), f = s.state({});
+  s.derive(() => {
+    const o = [], e = [], m = p.meshDensity.value.val, r = p.height.value.val, c = p.span.value.val, w = p.load.value.val;
+    o.push(...[
+      ...Array(m + 1).keys()
+    ].map((t) => [
+      0,
+      0,
+      r / m * t
+    ])), e.push(...[
+      ...Array(m).keys()
+    ].map((t) => [
+      t,
+      t + 1
+    ]));
+    let n = o.length;
+    o.push(...[
+      ...Array(m).keys()
+    ].map((t) => [
+      c / m * (t + 1),
+      0,
+      r
+    ])), e.push(...[
+      ...Array(m - 1).keys()
+    ].map((t) => [
+      n + t,
+      n + t + 1
+    ])), e.push([
+      n - 1,
+      n
+    ]), n = o.length;
+    const I = n - 1;
+    o.push(...[
+      ...Array(m).keys()
+    ].map((t) => [
+      c,
+      0,
+      r - r / m * (t + 1)
+    ])), e.push(...[
+      ...Array(m - 1).keys()
+    ].map((t) => [
+      n + t,
+      n + t + 1
+    ])), e.push([
+      n - 1,
+      n
+    ]);
+    const v = {
+      supports: /* @__PURE__ */ new Map([
+        [
+          0,
+          [
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+          ]
+        ],
+        [
+          o.length - 1,
+          [
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+          ]
+        ]
+      ]),
+      loads: /* @__PURE__ */ new Map([
+        [
+          I,
+          [
+            w,
+            0,
+            0,
+            0,
+            0,
+            0
+          ]
+        ]
+      ])
+    }, u = {
+      elasticities: new Map(e.map((t, a) => [
+        a,
+        10
+      ])),
+      shearModuli: new Map(e.map((t, a) => [
+        a,
+        10
+      ])),
+      areas: new Map(e.map((t, a) => [
+        a,
+        10
+      ])),
+      torsionalConstants: new Map(e.map((t, a) => [
+        a,
+        10
+      ])),
+      momentsOfInertiaY: new Map(e.map((t, a) => [
+        a,
+        10
+      ])),
+      momentsOfInertiaZ: new Map(e.map((t, a) => [
+        a,
+        10
+      ]))
+    }, y = b(o, e, v, u), M = O(o, e, u, y);
+    l.val = o, i.val = e, d.val = v, h.val = u, g.val = y, f.val = M;
+  });
+  document.body.append(_({
+    nodes: l,
+    elements: i,
+    nodeInputs: d,
+    elementInputs: h
+  }), S(p), k({
+    mesh: {
+      nodes: l,
+      elements: i,
+      nodeInputs: d,
+      elementInputs: h,
+      deformOutputs: g,
+      analyzeOutputs: f
+    },
+    settingsObj: {
+      deformedShape: true
+    }
+  }), A({
+    sourceCode: "https://github.com/madil4/awatif/blob/main/examples/src/1d-mesh/main.ts",
+    author: "https://www.linkedin.com/in/madil4/"
+  }));
+});
---
+++ assets/guerra-ej7-viga-cimentacion-new-C77obuLw.js
@@ -0,0 +1,39 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj7-R5TnvG1p.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-D5zRGavV.js";
+import "./theme-D5p5K0bJ.js";
+import { __tla as __tla_2 } from "./didacticCpp-C2di29sC.js";
+import { __tla as __tla_3 } from "./deform-D-nmLsId.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./getViewer-BhdEu_ed.js";
+import "./Text-B4nrRMfX.js";
+import "./styles-Bs20h4nQ.js";
+import "./units-Rdizb_dC.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
