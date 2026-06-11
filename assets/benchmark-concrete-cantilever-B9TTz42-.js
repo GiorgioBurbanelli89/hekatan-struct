9641d8e59e126aa4465a8042efb618332a76716e
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-11 00:35:44 -0500

    style(cad): planos de referencia mas transparentes

---
+++ assets/benchmark-concrete-cantilever-B9TTz42-.js
@@ -0,0 +1,49 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { b as r, __tla as __tla_0 } from "./benchmarkConcreteCantilever-Etzysc1d.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CwCLq-Qm.js";
+import { __tla as __tla_2 } from "./didacticCpp-C2di29sC.js";
+import { __tla as __tla_3 } from "./deform-D-nmLsId.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./units-DXgS9gy0.js";
+import "./theme-BUyDDEHW.js";
+import { __tla as __tla_4 } from "./cantileverE2k-B-y3HFc8.js";
+import "./analyze-DQyLT4zB.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import "./loadCaseHelpers-Cnzqw99W.js";
+import "./getViewer-Bth32GYc.js";
+import "./Text-DR6pe57W.js";
+import "./styles-tOu98xnK.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
---
+++ assets/zapata-viga-amarre-20uOIu4H.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { z as r, __tla as __tla_0 } from "./zapataVigaAmarre-C-Vq_cXc.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CwCLq-Qm.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./theme-BUyDDEHW.js";
+import "./analyze-DQyLT4zB.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-C2di29sC.js";
+import { __tla as __tla_3 } from "./deform-D-nmLsId.js";
+import "./exampleVersion-D1A_5i59.js";
+import "./getViewer-Bth32GYc.js";
+import "./Text-DR6pe57W.js";
+import "./styles-tOu98xnK.js";
+import "./units-DXgS9gy0.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
