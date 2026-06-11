022e1fcdc19e4305fd153d1a70894b038ced86d8
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-11 00:07:37 -0500

    feat(cad): area rectangular (2 clics) funciona en plano inclinado -> rectangulo limpio

---
+++ assets/benchmark-cft-CnUpQqfN.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { b as r, __tla as __tla_0 } from "./benchmarkCft-BKjOhX0H.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-Dt4zfs4e.js";
+import "./analyze-DQyLT4zB.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-C2di29sC.js";
+import { __tla as __tla_3 } from "./deform-D-nmLsId.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./theme-BUyDDEHW.js";
+import "./getViewer-D5wKgJ7m.js";
+import "./Text-DR6pe57W.js";
+import "./styles-tOu98xnK.js";
+import "./units-DXgS9gy0.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
