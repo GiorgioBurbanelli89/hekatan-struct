3b6b76fe91cfaffd12845d595e335cc5a503d290
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-10 22:52:27 -0500

    fix(cad): Delete borra la seleccion aunque la barra de comandos este enfocada (vacia)

---
+++ assets/zapata-aislada-validacion-CxAR2pZn.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { z as o, __tla as __tla_0 } from "./zapataAisladaValidacion-CHdqKjhY.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-Ci5DxdnD.js";
+import "./theme-D5p5K0bJ.js";
+import "./analyze-DQyLT4zB.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-C2di29sC.js";
+import { __tla as __tla_3 } from "./deform-D-nmLsId.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./exampleVersion-D1A_5i59.js";
+import "./units-Rdizb_dC.js";
+import "./getViewer-CNBPpMc3.js";
+import "./Text-B4nrRMfX.js";
+import "./styles-Bs20h4nQ.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(o);
+});
