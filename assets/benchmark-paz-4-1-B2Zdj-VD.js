2db5ee5f35f8dd699ba757afbc2004c54629d6e9
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-23 01:16:54 -0500

    fix móvil dropdown: select touch-action none (pan-cancel cerraba el picker) + ejes viga

---
+++ assets/benchmark-paz-4-1-B2Zdj-VD.js
@@ -0,0 +1,49 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { b as r, __tla as __tla_0 } from "./benchmarkPaz4_1-QIjvtNfd.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-C3qGIHK0.js";
+import { __tla as __tla_2 } from "./pazFrameE2k-BnbkYaHT.js";
+import "./analyze-CWooBpVN.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_3 } from "./didacticCpp-C0Qfkfmr.js";
+import { __tla as __tla_4 } from "./deform-yikvckPF.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./newmarkBeta-CfFoMJcW.js";
+import "./theme-BUyDDEHW.js";
+import "./getViewer-BP1guy5F.js";
+import "./Text-DR6pe57W.js";
+import "./styles-tOu98xnK.js";
+import "./units-DXgS9gy0.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
---
+++ assets/membrana-csi-zboiNbzn.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { m, __tla as __tla_0 } from "./membranaCSI-CQS27YMY.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-C3qGIHK0.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./analyze-CWooBpVN.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-C0Qfkfmr.js";
+import { __tla as __tla_3 } from "./deform-yikvckPF.js";
+import "./theme-BUyDDEHW.js";
+import "./getViewer-BP1guy5F.js";
+import "./Text-DR6pe57W.js";
+import "./styles-tOu98xnK.js";
+import "./units-DXgS9gy0.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(m);
+});
