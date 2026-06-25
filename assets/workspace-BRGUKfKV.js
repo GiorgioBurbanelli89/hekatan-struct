2d85d27f561dd0d6eb96562e73b7e6bfa9d4cc8c
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-25 18:49:33 -0500

    deploy: selector caso -> Case results

--- assets/workspace-BRGUKfKV.js
+++ assets/workspace-BRGUKfKV.js
@@ -5976,7 +5976,7 @@ ${_}
       }, _ = {};
       a.val.forEach((v) => _[v.name] = v.name), Object.keys(_).length === 0 && (_["(ninguno)"] = "");
       const p = d.addBinding(M, "case", {
-        label: "\u25B6 Result case",
+        label: "Case results",
         options: _
       });
       try {
