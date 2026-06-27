4af0a7dc5243a7fc62b1d68aa6d2cea8745869d3
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-27 10:12:25 -0500

    deploy: restaurar colormap CSI

---
+++ assets/triangular-plate-DOwP6D-i.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { t as r, __tla as __tla_0 } from "./triangularPlate-CSV015_V.js";
+import { r as t, __tla as __tla_1 } from "./runExampleStandalone-BK1Rk3-u.js";
+import { __tla as __tla_2 } from "./didacticCpp-q5lN0Q74.js";
+import { __tla as __tla_3 } from "./deform-Cj9M_0na.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./mitc3-2FJr2z_r.js";
+import "./theme-Cr2LU0HL.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-KNQQX4K0.js";
+import "./Text-BbGxMO8j.js";
+import "./styles-ChEEn6BP.js";
+import "./units-VZqt9QNE.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  t(r);
+});
