bf9ba35f81e2610fa5b64c8dffb11c4af3c4869f
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-06-27 10:06:04 -0500

    deploy: colormap SAFE

---
+++ assets/cad-draw-CixG7jny.js
@@ -0,0 +1,11 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { c as r } from "./cadDraw-D1zgZQaH.js";
+import { r as m } from "./runExampleStandalone-CAse6Hi6.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./theme-Cr2LU0HL.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B3YxfX60.js";
+import "./Text-BbGxMO8j.js";
+import "./styles-ChEEn6BP.js";
+import "./units-VZqt9QNE.js";
+m(r);
---
+++ assets/guerra-ej1-zapata-cuadrada-CETKC6FQ.js
@@ -0,0 +1,40 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj1-B0SEY3Qg.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CAse6Hi6.js";
+import "./theme-Cr2LU0HL.js";
+import { __tla as __tla_2 } from "./didacticCpp-q5lN0Q74.js";
+import { __tla as __tla_3 } from "./deform-Cj9M_0na.js";
+import "./preload-helper-DrUBW0xl.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B3YxfX60.js";
+import "./Text-BbGxMO8j.js";
+import "./styles-ChEEn6BP.js";
+import "./units-VZqt9QNE.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
