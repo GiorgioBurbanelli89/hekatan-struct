d5b24bc545829ddd50260f4083c9b513d9847b42
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-08-16 01:06:10 -0500

    deploy: secciones por forma y dimensiones + dos edificios que no resolvian

---
+++ assets/edificio-muros-LL-cs030.js
@@ -0,0 +1,51 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { e as o, __tla as __tla_0 } from "./edificioMuros-a26ebobY.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-qKp-5K7y.js";
+import { __tla as __tla_2 } from "./edificioAporticado-CoYEUBwb.js";
+import "./analyze-Baqb28rE.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_3 } from "./didacticCpp-PqvqKlgs.js";
+import { __tla as __tla_4 } from "./deform-DDFJ5ylS.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./cotas3D-BRJLBeVj.js";
+import "./theme-Co6w-pfC.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-DvLc7YHY.js";
+import "./Text-2W5davkr.js";
+import "./styles-CcTqhjpF.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-DErH5pWJ.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(o);
+});
