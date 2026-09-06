804d025add83cd2605ea4defa046dbc867d3f727
GiorgioBurbanelli89 <giorgio@example.com>
2026-09-06 02:21:44 -0500

    Deploy 6-sep-2026: modal sin cuelgue al arrastrar, colormap robusto en muros, leyenda corta, viewFrom test-m-dual, sin ping a Ollama

---
+++ assets/guerra-ej6-zapata-unida-viga-amarre-DHY6RigM.js
@@ -0,0 +1,51 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj6-BDUNRMVp.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-BFMU0gTk.js";
+import { __tla as __tla_2 } from "./zapataVigaAmarre-CjnZ1Hh4.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./theme-Co6w-pfC.js";
+import "./analyze-BFwM3Jvn.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_3 } from "./didacticCpp-DaEmtxPu.js";
+import { __tla as __tla_4 } from "./deform-ZnZ8PQ4z.js";
+import "./exampleVersion-D1A_5i59.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B8CqO2_V.js";
+import "./Text-2W5davkr.js";
+import "./styles-DrkfbexP.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-uEM3KxcA.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_4;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
